706f161c29e883d9f58e38a46a8bc7724f2b73de
Vadym Tyemirov <temirov@gmail.com>
2014-04-28 08:33:11 +0700

    Facebook credentials for Heroku url added

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "268821136633495", "e79acf66e6ff9d90dd35991f009a6b62"
+  # config.omniauth :facebook, "268821136633495", "e79acf66e6ff9d90dd35991f009a6b62"
+  config.omniauth :facebook, Rails.application.secrets.facebook["app_id"], Rails.application.secrets.facebook["app_secret"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
